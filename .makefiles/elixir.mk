d7eb1138f74882b87075ce79c096b731a5a4ead8
Tyr Chen <tyr.chen@gmail.com>
2018-07-22 21:26:44 -0700

    compile for all target

--- .makefiles/elixir.mk
+++ .makefiles/elixir.mk
@@ -3,7 +3,7 @@ RELEASE_DIR=$(TOP_DIR)/_build/releases
 
 build-all:
 	@echo "Building all dependencies..."
-	@mix deps.get; MIX_ENV=prod mix compile; MIX_ENV=staging mix compile;
+	@mix deps.get; MIX_ENV=prod mix compile; MIX_ENV=staging mix compile; MIX_ENV=dev mix compile; MIX_ENV=test mix compile;
 	@tar zcvf deps.tgz deps/
 	@tar zcvf builds.tgz _build/
 	@mv *.tgz $(RELEASE_DIR)
