bb0eb9cbb9c589e5c8c8d3a365e2bbb9df8bc80a
sunboshan <sunboshan@users.noreply.github.com>
2018-11-26 21:29:43 -0800

    Fix duplicate versions (#14)
    * Fix duplicate versions
    
    * bump version

--- .makefiles/elixir.mk
+++ .makefiles/elixir.mk
@@ -8,9 +8,9 @@ build-all:
 	@echo "Building dependencies for MIX_ENV=staging..."; MIX_ENV=staging mix compile
 	@echo "Building dependencies for MIX_ENV=dev..."; MIX_ENV=dev mix compile
 	@echo "Building dependencies for MIX_ENV=test..."; MIX_ENV=test mix compile
+	@rm $(RELEASE_DIR)/v*
 	@tar zcf deps.tgz deps/
 	@tar zcf builds.tgz _build/
-	@rm $(RELEASE_DIR)/v*
 	@mv *.tgz $(RELEASE_DIR)
 
 build-version-file:
